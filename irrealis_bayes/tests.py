5e490babe371e6bcb5a33cd63b037cd945aeb5dd
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2013-12-30 13:38:13 -0600

    Removing test that fails on purpose, having verified that failures are detected.

--- irrealis_bayes/tests.py
+++ irrealis_bayes/tests.py
@@ -6,8 +6,5 @@ class UnitTestHello(unittest.TestCase):
   def test_hello(self):
     self.assertTrue(True)
 
-  def test_fail(self):
-    self.assertTrue(False)
-
 
 if __name__ == "__main__": unittest.main()
